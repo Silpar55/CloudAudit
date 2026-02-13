08658f093a1bcd803145cee7947e9141cf5fcee9
asilva-juarez <asilva-juarez@myseneca.ca>
2026-02-13 18:08:35 -0500

    Added nightly cron job for cost explorer call

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import app from "#app";
 import { verifyAwsConnection } from "#utils/aws/sts.js";
 import { verifyDatabaseConnection } from "#config";
+import { startNightlyCostJob } from "./jobs/nightlyCost.job.js";
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, async () => {
   await verifyDatabaseConnection();
   await verifyAwsConnection();
+  startNightlyCostJob();
   console.log("App listening on port", port);
 });
